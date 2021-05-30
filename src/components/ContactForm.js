608b91aa6e55580c271143f157d65be0216c29d7
TomUglow <tom.uglow@hotmail.com>
2021-05-30 13:38:12 +1000

    Email Submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,59 +1,68 @@
 import React from 'react';
 import '../components/ContactForm.css';
 import LocationPin from '..//images/location-pin.png';
+import emailjs, { init } from 'emailjs-com';
+init("user_kOD2awCiUQTvoAfY4hMUn");
 
 class ContactForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        username: '',
-        age: null,
-        errormessage: ''
+        name: '',
+        email: '',
+        description: ''
         };
+    this.handleSubmit = this.handleSubmit.bind(this);
     }
-    myChangeHandler = (event) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        let err = '';
-        if (nam === "age") {
-        if (val !="" && !Number(val)) {
-            err = <strong className='form-error-message'>*You must enter a valid email address</strong>;
-        }
-        }
-        this.setState({errormessage: err});
-        this.setState({[nam]: val});
-    }
+
+    handleSubmit() {
+    const btn = document.getElementById('form-submit-button');
+
+    document.getElementById('form')
+        .addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+    btn.value = 'Sending...';
+    
+    const serviceID = 'default_service';
+    const templateID = 'template_2s56c7v';
+    
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+        btn.value = 'Send Email';
+            alert('Sent!');
+        }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+        });
+    });        
+};
+    
     render() {
         return (
-        <form className='contact-form'> 
+        <>
             <h1 className='form-heading'>Get In Touch {this.state.username} </h1>
             <h2 className='form-mobile'>Call Ben on 0411 111 111 or fill in the form below</h2>
             <h3 className='form-location'><img className='location-pin' src={LocationPin} alt='Location Pin Icon' width="13" height="20" /> Based in Melbourne</h3>
-            <p className='form-question1'>Enter your name:</p>
-            <input
-                className='form-box1'
-                type='text'
-                name='username'
-                onChange={this.myChangeHandler}
-            />
-            <p className='form-question2'>Enter your email:</p>
-            <input
-                className='form-box2'
-                type='text'
-                name='age'
-                onChange={this.myChangeHandler}
-            />
-            <p className='form-question3'>How can we help?</p>
-            <textarea
-                className='form-box3'
-                name='Description'
-                onChange={this.myChangeHandler}
-            />
-            {this.state.errormessage}
-        </form>
+            
+            <form id="form" className="form">
+                <label for="name" className="form-name">Enter your name:</label>
+                <input type="text" name="name" id="name" className="form-box1"/>
+
+                <label for="email" className="form-email">Enter your email address:</label>
+                <input type="text" name="email" id="email" className="form-box2"/>
+
+                <label for="description" className="form-description">How can we help?</label>
+                <textarea type="text" name="description" id="description" className="form-box3"/>
+
+                <input type="submit" id="form-submit-button" value="Submit Form" className="form-submit" onClick={this.handleSubmit}/>
+            </form>
+
+            <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
+            <script type="text/javascript"> emailjs.init('user_kOD2awCiUQTvoAfY4hMUn')</script>
+        </>
         );
     }
 }
-
 
   export default ContactForm;
